a36d7807bec52e827a105cd42ff6117df4dc549f
mania <160741423+Maniaali@users.noreply.github.com>
2024-03-26 01:01:36 +0500

    Pushing my Cli Num gussing game on git

---
+++ index.js
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "usergussedNumber",
+        type: "number",
+        message: "please guess the Number between 1 to 10",
+    },
+]);
+if (answers.usergussedNumber === randomnumber) {
+    console.log("congratulations! you gussed the right number");
+}
+else {
+    console.log("lose!you gussed the wroung number");
+}
+console.log(answers);
